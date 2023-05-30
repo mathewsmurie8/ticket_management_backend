cbfdd100307d45b1e143797f4e5fe7739b29f157
mathewsmurie8 <mathewsmurie@gmail.com>
2023-05-30 22:44:04 +0300

    Add todo

--- crud/config/settings.py
+++ crud/config/settings.py
@@ -132,6 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+# todo: Make the default primary key a uuid
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
